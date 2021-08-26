3f3e9b684f5223ef2de3cee8e93ad7568bec34bf
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-08-26 15:23:39 -0300

    :art: update mongodb connnection-string

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const dbUser = {
   name: "admin",
   password: "admin0112358",
+  defaultDB: "billy-dev",
 };
 
-export const dbUrl = `mongodb+srv://${dbUser.name}:${dbUser.password}@cluster0.syhbj.mongodb.net/billy-dev?retryWrites=true&w=majority`;
+export const dbUrl = `mongodb://${dbUser.name}:${dbUser.password}@cluster0-shard-00-00.syhbj.mongodb.net:27017,cluster0-shard-00-01.syhbj.mongodb.net:27017,cluster0-shard-00-02.syhbj.mongodb.net:27017/${dbUser.defaultDB}?ssl=true&replicaSet=atlas-oj0tdj-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 export const database = () => {
   mongoose.Promise = global.Promise;
